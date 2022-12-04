b8fc2ff964eeee2a241d65bcb269779b18885d55
Magnús Ólafsson <magnusol93@gmail.com>
2022-12-04 17:38:36 -0500

    Config lsp saga

--- config/lspsaga.lua
+++ config/lspsaga.lua
@@ -6,30 +6,27 @@ local config = {noremap = true, silent = true}
 local NORMAL_MODE = "n"
 
 saga.init_lsp_saga({
-    -- Options with default value
-    -- "single" | "double" | "rounded" | "bold" | "plus"
-    border_style = "rounded",
+    border_style = "rounded", -- "single" | "double" | "rounded" | "bold" | "plus"
+
     -- the range of 0 for fully opaque window (disabled) to 100 for fully
     -- transparent background. Values between 0-30 are typically most useful.
     saga_winblend = 0,
-    symbol_in_winbar = {in_custom = true},
-    -- when cursor in saga window you config these to move
-    move_in_saga = {prev = '<C-p>', next = '<C-n>'},
-    -- Error, Warn, Info, Hint
-    -- use emoji like
-    -- { "🙀", "😿", "😾", "😺" }
-    -- or
-    -- { "😡", "😥", "😤", "😐" }
-    -- and diagnostic_header can be a function type
-    -- must return a string and when diagnostic_header
-    -- is function type it will have a param `entry`
-    -- entry is a table type has these filed
-    -- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
-    diagnostic_header = {" ", " ", " ", "ﴞ "},
+    symbol_in_winbar = {
+        in_custom = true,
+        enable = true,
+        separator = ' > ',
+        show_file = true,
+        file_formatter = "",
+        click_support = false
+    },
+
+    move_in_saga = {prev = '<C-p>', next = '<C-n>'}, -- when cursor in saga window you config these to move
+
+    diagnostic_header = {"", "", "", "ﴞ"}, -- Error, Warn, Info, Hint
     -- preview lines of lsp_finder and definition preview
     max_preview_lines = 10,
     -- use emoji lightbulb in default
-    code_action_icon = "💡",
+    code_action_icon = "ﴞ",
     -- if true can press number to execute the codeaction in codeaction window
     code_action_num_shortcut = true,
     -- same as nvim-lightbulb but async
@@ -54,17 +51,21 @@ saga.init_lsp_saga({
     rename_in_select = true,
     show_outline = {
         win_position = 'right',
+        -- set special filetype win that outline window split.like NvimTree neotree
+        -- defx, db_ui
+        win_with = '',
+        win_width = 30,
         auto_enter = true,
         auto_preview = true,
-        virt_text = '┃',
+        virt_text = '|',
         jump_key = '<cr>',
         -- auto refresh when change buffer
         auto_refresh = true
     }
 })
 
-map(NORMAL_MODE, "gd", "<cmd>Lspsaga peek_definition<CR>", config)
-map(NORMAL_MODE, "<leader>o", "<cmd>LSoutlineToggle<CR>", config)
+map(NORMAL_MODE, "gD", "<cmd>Lspsaga peek_definition<CR>", config)
+map(NORMAL_MODE, "gd", vim.lsp.buf.definition, config)
 map(NORMAL_MODE, "gr", "<cmd>Lspsaga lsp_finder<CR>", config)
 map(NORMAL_MODE, "K", "<cmd>Lspsaga hover_doc<CR>", config)
 map(NORMAL_MODE, "<leader>rn", "<cmd>Lspsaga rename<CR>", config)
@@ -72,10 +73,13 @@ map(NORMAL_MODE, "<leader>ca", "<cmd>Lspsaga code_action<CR>", config)
 map(NORMAL_MODE, "<leader>E", "<cmd>Lspsaga show_line_diagnostics<CR>", config)
 map(NORMAL_MODE, "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", config)
 map(NORMAL_MODE, "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", config)
+map(NORMAL_MODE, "<leader>o", "<cmd>LSoutlineToggle<CR>", config)
 
 -- Example:
 local function get_file_name(include_path)
+    ---@diagnostic disable-next-line: missing-parameter
     local file_name = require('lspsaga.symbolwinbar').get_file_name()
+
     if vim.fn.bufname '%' == '' then return '' end
     if include_path == false then return file_name end
     -- Else if include path: ./lsp/saga.lua -> lsp > saga.lua
@@ -109,8 +113,6 @@ local function config_winbar_or_statusline()
         win_val = get_file_name(true) -- set to true to include path
         if sym ~= nil then win_val = win_val .. sym end
         vim.wo.winbar = win_val
-        -- if work in statusline
-        vim.wo.stl = win_val
     end
 end
 
--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 for k, _ in pairs(package.loaded) do
-    if string.match(k, "config/utils") then package.loaded[k] = nil end
+    if string.match(k, "config/") then package.loaded[k] = nil end
 end
 
 require "config.basic"
