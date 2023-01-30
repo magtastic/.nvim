2f6425f4bac8cdfb538fe1de3d56c3a935f4c751
Magnús Ólafsson <magnusol93@gmail.com>
2023-01-30 14:44:36 +0000

    Add treesitter comment

--- config/treesitter.lua
+++ config/treesitter.lua
@@ -6,4 +6,18 @@ treesitter.setup {
     highlight = {enable = true, additional_vim_regex_highlighting = false},
     indent = {enable = true},
     autotag = {enable = true}
+    -- textobjects = {
+    -- TODO: check this out!
+    --     enable = true,
+    --     lsp_interop = {enable = true},
+    --     keymaps = {["af"] = "@arrow_function", ["if"] = "@arrow_function"},
+    --     select = {
+    --         enable = true,
+    --         keymaps = {
+    --             -- You can use the capture groups defined in textobjects.scm
+    --             ["af"] = "@arrow_function",
+    --             ["if"] = "@arrow_function"
+    --         }
+    --     }
+    -- }
 }
